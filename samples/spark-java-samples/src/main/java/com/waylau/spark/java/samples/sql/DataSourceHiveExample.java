632da6a87f23a65bb42901afc3944885b7ed3eb5
liuweiwei <liuweiwei@iscmtech.com>
2024-05-26 16:46:22 +0800

    add Hive and ORC demos

--- samples/spark-java-samples/src/main/java/com/waylau/spark/java/samples/sql/DataSourceHiveExample.java
+++ samples/spark-java-samples/src/main/java/com/waylau/spark/java/samples/sql/DataSourceHiveExample.java
@@ -4,8 +4,9 @@
 
 package com.waylau.spark.java.samples.sql;
 
-import org.apache.spark.sql.SparkSession;
 import java.io.File;
+
+import org.apache.spark.sql.SparkSession;
 
 /**
  * DataSource with Hive Example.
@@ -18,23 +19,26 @@ public class DataSourceHiveExample {
     public static void main(String[] args) {
         // warehouseLocation 指向托管数据库和表的仓库位置
         String warehouseLocation = new File("spark-warehouse").getAbsolutePath();
-     
+
         SparkSession sparkSession = SparkSession.builder()
-            .appName("DataSourceHive") // 设置应用名称
-            .master("local") // 本地单线程运行
-            .config("spark.sql.warehouse.dir", warehouseLocation) // 设置仓库位置
-            .enableHiveSupport() // 启用Hive支持
-            .getOrCreate();
+            // 设置应用名称
+            .appName("DataSourceHiveExample")
+            // 本地单线程运行
+            .master("local")
+            // 设置仓库位置
+            .config("spark.sql.warehouse.dir", warehouseLocation)
+            // 启用Hive支持
+            .enableHiveSupport().getOrCreate();
 
         // 建表
         sparkSession.sql("CREATE TABLE IF NOT EXISTS src (key INT, value STRING) USING hive");
 
         // 从本地文件kv1.txt加载数据
-        sparkSession.sql("LOAD DATA LOCAL INPATH 'src/main/resources/kv1.txt' INTO TABLE src");
-     
+        sparkSession.sql("LOAD DATA LOCAL INPATH 'kv1.txt' INTO TABLE src");
+
         // 查询
         sparkSession.sql("SELECT * FROM src").show();
-       
+
         // 关闭SparkSession
         sparkSession.stop();
     }
---
+++ samples/spark-java-samples/src/main/java/com/waylau/spark/java/samples/sql/DataSourceOrcExample.java
@@ -0,0 +1,69 @@
+/**
+ * Welcome to https://waylau.com
+ */
+package com.waylau.spark.java.samples.sql;
+
+import java.util.List;
+
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.RowFactory;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.types.Metadata;
+import org.apache.spark.sql.types.StructField;
+import org.apache.spark.sql.types.StructType;
+
+/**
+ * DataSource with ORC Example.
+ *
+ * @author <a href="https://waylau.com">Way Lau</a>
+ * @since 2024-05-26
+ */
+public class DataSourceOrcExample {
+
+    public static void main(String[] args) {
+        SparkSession sparkSession = SparkSession.builder()
+            // 设置应用名称
+            .appName("DataSourceOrcExample")
+            // 本地单线程运行
+            .master("local").getOrCreate();
+
+        // 构造数据
+        List<Row> data = List.of(RowFactory.create(1, "John Doe", 30), RowFactory.create(2, "Jane Smith", 25));
+
+        // 构造模式
+        StructType schema =
+            new StructType(new StructField[] {new StructField("id", DataTypes.IntegerType, false, Metadata.empty()),
+                new StructField("name", DataTypes.StringType, false, Metadata.empty()),
+                new StructField("age", DataTypes.IntegerType, false, Metadata.empty())});
+
+        // 创建一个简单的DataFrame
+        Dataset<Row> createDf = sparkSession.createDataFrame(data, schema);
+
+        // 将DataFrame写入ORC文件
+        createDf.write()
+            // 文件格式ORC
+            .format("orc")
+            // 指定保存文件的路径
+            .save("output/users_age.orc");
+
+        // 创建DataFrame
+        // 返回一个DataFrameReader，可用于将非流数据作为DataFrame读取
+        Dataset<Row> readDf = sparkSession.read()
+            // 文件格式ORC
+            .format("orc")
+            // 从指定路径读取文件
+            .load("output/users_age.orc");
+
+        // 将DataFrame的内容显示
+        readDf.show();
+
+        // 打印schema
+        readDf.printSchema();
+
+        // 关闭SparkSession
+        sparkSession.stop();
+    }
+
+}
